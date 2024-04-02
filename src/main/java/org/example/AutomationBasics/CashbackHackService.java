3013a17c9aa04a361451f4ff0cf7986e069c8464
Aleks test <7501130l@gmail.com>
2024-04-02 22:21:32 +0300

    Initial commit

---
+++ src/main/java/org/example/AutomationBasics/CashbackHackService.java
@@ -0,0 +1,8 @@
+package org.example.AutomationBasics;
+public class CashbackHackService {
+    private final int boundary = 1000;
+
+    public int remain(int amount) {
+        return boundary - amount % boundary;
+    }
+}
